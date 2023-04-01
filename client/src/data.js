fae318d5815af50e7aa890438d8877dbd6ed4aba
Almir Međuseljac <78498487+cico-cicko@users.noreply.github.com>
2023-04-01 17:26:38 +0200

    Add files via upload

---
+++ client/src/data.js
@@ -0,0 +1,82 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://m.media-amazon.com/images/I/61BMCPTLLvL._SX679_.jpg",
+      title: "GAMES AND ACCESSORIES",
+      desc: "Explore our shoes category in Catalog and find your best pair!",
+      bg: "fff",
+      cat:"accessories",
+    },
+    {
+      id: 2,
+      img: "https://png.pngtree.com/element_our/png/20181129/computer-desktop-wallpaper-background-png_246987.jpg",
+      title: "PC AND CONSOLE",
+      desc: "Looking for a new PC or Gamnig Console. Explore the shop.",
+      bg: "fff",
+      cat:"pc-console",
+    },
+    {
+      id: 3,
+      img: "https://media.4rgos.it/i/Argos/7033252_R_Z001A?w=1500&h=880&qlt=70&fmt=webp",
+      title: "BOARD GAMES",
+      desc: "You like playing board games with your friends? Explore the shop.",
+      bg: "fff",
+      cat:"board-games",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.asos-media.com/products/violet-romance-quilted-longline-coat-in-khaki/202706693-1-khaki?$n_640w$&wid=513&fit=constrain",
+      title: "WOMAN",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/736x/27/a9/44/27a94446a57f6e052b3fd75f7f8f1405.jpg",
+      title: "KIDS",
+      cat:"kids"
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/originals/4d/13/da/4d13da0f2fd161fe399aaeab9b93f2c9.jpg",
+      title: "MAN",
+      cat:"man"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
